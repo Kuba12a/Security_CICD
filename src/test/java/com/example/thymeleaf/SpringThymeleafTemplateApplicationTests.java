546b01058eaca3bbaf823114fe295ce9a0b02baf
Kuba12a <kaluski1212@wp.pl>
2025-01-19 20:33:13 +0100

    Update test

--- src/test/java/com/example/thymeleaf/SpringThymeleafTemplateApplicationTests.java
+++ src/test/java/com/example/thymeleaf/SpringThymeleafTemplateApplicationTests.java
@@ -3,11 +3,13 @@ package com.example.thymeleaf;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SpringThymeleafTemplateApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void AlwaysTrueTest() {
+		assertTrue(1 == 1);
 	}
-
 }
